c31cb3239f2f54534f4410aa60310bac06fa7627
tyao117 <tyao117@gmail.com>
2019-01-29 10:04:29 -0800

    second day, added templates and basic configs

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'Tim'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        }, 
+        {
+            'author': {'username': 'Blazer'},
+            'body': 'Lolz, this blog is too basic'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
